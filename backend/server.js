061333a7c7f7d6b34211035c9f96c3d535208ecb
khalilzitouni <114951550+khalilzitouni2058@users.noreply.github.com>
2025-02-02 16:41:38 +0100

    added the login UI and backend login/signup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);   
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- frontend-react-native/life-at-smu/App.js
+++ frontend-react-native/life-at-smu/App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Home from './features/HomePage/screens/Home';
+import Login from './features/HomePage/screens/Login';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
+
+const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home}  options={{ headerShown: false }} />
+      <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
