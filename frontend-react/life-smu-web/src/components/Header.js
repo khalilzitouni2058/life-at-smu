07df6f2371b88d6a47bfe17ff266209f70912349
Alaaeddine bousselmi <alaaeddine.bousselmi@medtech.tn>
2025-04-09 13:34:53 +0100

    header before add-club

--- frontend-react/life-smu-web/src/components/Header.js
+++ frontend-react/life-smu-web/src/components/Header.js
@@ -1,27 +1,18 @@
-import { Box, Flex, Heading, Button, HStack, useBreakpointValue } from "@chakra-ui/react";
+import React from 'react';
+import {
+  CHeader,
+  CContainer,
+  CHeaderBrand,
+} from '@coreui/react';
+import '../styles/Dashboard/Header.css'; 
 
 function Header() {
-  // Handle responsive design with `useBreakpointValue`
-  const isMobile = useBreakpointValue({ base: true, md: false });
-
   return (
-    <Box as="header" bg="blue.200" color="white" p={4}>
-      <Flex justify="space-between" align="center">
-        {/* Logo */}
-        <Heading size="lg" letterSpacing="wider">
-          MyLogo
-        </Heading>
-
-        {/* Navigation */}
-        
-        
-
-        {/* Call-to-Action Button */}
-        <Button colorScheme="teal" variant="solid">
-          Get Started
-        </Button>
-      </Flex>
-    </Box>
+    <CHeader  className="custom-header ">
+      <CContainer fluid>
+        <CHeaderBrand>Header </CHeaderBrand>
+      </CContainer>
+    </CHeader>
   );
 }
 
